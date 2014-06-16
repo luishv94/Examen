2993c871f9006420090e57b406d29d1d4e87076f
Luis <luishv94@gmail.com>
2014-06-16 07:53:09 -0500

    Examen Final funcionando.

---
+++ WebContent/Resta.jsp
@@ -0,0 +1,47 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Resta</title>
+</head>
+<body>
+<script>
+function validation(){
+	var campo1=document.forms["Resta"]["campo1"].value;
+	var campo2=document.forms["Resta"]["campo2"].value;
+	if(!campo1){
+		alert("Campo 1 vacío");
+		return false;
+	}else if(!campo2){
+		alert("Campo 2 vacío");
+		return false;
+	}
+	return true;
+}
+</script>
+<form name="Resta" method="post" action="ServletResta" onsubmit="return validation()">
+<table>
+<tr>
+	<td>Primer Numero</td>
+	<td>
+	<input type="text" name="campo1">
+	</td>
+	</tr>
+	<tr>
+	<td>Segundo Numero</td>
+	<td>
+	<input type="text" name="campo2">
+	</td>
+	</tr>
+	<tr>
+	<td>
+	<input type="submit" name="calcular" value="Calcular">
+	</td>
+	</tr>
+</table>
+</form>
+
+</body>
+</html>
---
+++ src/ServletMultiplicacion.java
@@ -0,0 +1,45 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletMultiplicacion
+ */
+public class ServletMultiplicacion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletMultiplicacion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
+		response.setContentType("text/html");
+		int campo1,campo2,resultado;
+		campo1=Integer.parseInt(request.getParameter("campo1"));
+		campo2=Integer.parseInt(request.getParameter("campo2"));
+		resultado=campo1*campo2;
+		writer.print("<html><body> <h3>El resultado es: "+resultado+"</h3></html></body>");
+	}
+
+}
